03698fe1aa22f2b9abfa2ff3ff2acc268e84367b
3355844 <3355844@gmail.com>
2017-01-15 13:38:36 +0200

    process SaveTo File

--- AnnotationReflection_Example_2/src/Saver.java
+++ AnnotationReflection_Example_2/src/Saver.java
@@ -1,10 +1,15 @@
+import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Created by 33558 on 13.01.2017.
  */
 public class Saver {
 
     @SaverMethod
-    public void save(TextContainer container) {
+    public  void save(TextContainer container) {
         System.out.println("Come to save Method");
         Class<?> containerClass = container.getClass();
         if (!containerClass.isAnnotationPresent(SaveTo.class)){
@@ -13,6 +18,17 @@ public class Saver {
             System.out.println("Come to annotated Class SaveTo...");
             String path = containerClass.getAnnotation(SaveTo.class).path();
             System.out.println(path);
+            try {
+                ObjectOutputStream oos =  new ObjectOutputStream(new FileOutputStream(path, true ));
+                Method method = containerClass.getMethod("getStringTextContainer");
+                String text = (String) method.invoke(TextContainer.class.newInstance());
+                System.out.println(text);
+                oos.writeObject(text);
+                oos.flush();
+                oos.close();
+            } catch (IOException | IllegalAccessException | NoSuchMethodException | InvocationTargetException | InstantiationException e) {
+                e.printStackTrace();
+            }
         }
     }
 
--- AnnotationReflection_Example_2/src/TextContainer.java
+++ AnnotationReflection_Example_2/src/TextContainer.java
@@ -1,7 +1,7 @@
 /**
  * Created by 33558 on 13.01.2017.
  */
-@SaveTo(path = "c:\\dbFile.txt")
+@SaveTo(path = "c:\\file.txt")
 public class TextContainer {
 
     private String stringTextContainer = "String Text Container`s";
